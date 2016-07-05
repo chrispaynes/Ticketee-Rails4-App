b3b6fe9b69223fda025a073c91d6a30303b9341b
chrispaynes <cdpaynes@gmail.com>
2016-07-04 20:33:54 -0500

    Adds sign in and sign out capabilities

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -47,4 +47,8 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
+  
+  # allows Warden to use feature specs and reset itself after each use
+  config.include Warden::Test::Helpers, type: :feature
+  config.after(type: :feature) { Warden.test_reset! }
 end
