c356590e2262759c654cf2a9baad1eeeb11c7db9
chrispaynes <cdpaynes@gmail.com>
2016-07-09 23:22:46 -0500

    Hides project listing from users that don't have permission to see them

--- app/policies/project_policy.rb
+++ app/policies/project_policy.rb
@@ -1,7 +1,12 @@
 class ProjectPolicy < ApplicationPolicy
   class Scope < Scope
+    # defines the project scope depending on a user's role.
     def resolve
-      scope
+      return scope.none if user.nil?
+      return scope.all if user.admin?
+
+      # uses SQL INNER JOIN to return all projects for signed in users
+      scope.joins(:roles).where(roles: {user_id: user})
     end
   end
 
