863f007ab9aab1f376d2b75e82813bfb5f5b432d
chrispaynes <cdpaynes@gmail.com>
2016-07-22 20:33:32 -0500

    Users can tag tickets upon creation

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -55,7 +55,7 @@ before_action :set_ticket, only: [:show, :edit, :update, :destroy]
   private
 
   def ticket_params
-    params.require(:ticket).permit(:name, :description,
+    params.require(:ticket).permit(:name, :description, :tag_names,
       attachments_attributes: [:file, :file_cache])
   end
 
