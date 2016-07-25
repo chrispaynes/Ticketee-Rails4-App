0412636ad317793b9e14182bad449681266f2149
chrispaynes <cdpaynes@gmail.com>
2016-07-25 16:25:35 -0500

    Restricts tagging to only authorized users

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -16,11 +16,26 @@ RSpec.describe CommentsController, type: :controller do
     end
 
     it "cannot transition a state by passing through state_id" do
-      post :create, { comment: { text: "Did I hack it??",
+      post :create, { comment: { text: "Did I hack it?",
                                  state_id: state.id},
                       ticket_id: ticket.id }
       ticket.reload
       expect(ticket.state).to be_nil
+    end
+  end
+
+  context "a user without permission to tag a ticket" do
+    before do
+      assign_role!(user, :editor, project)
+      sign_in user
+    end
+
+    it "cannot tag a ticket when creating a comment" do
+      post :create, { comment: { text: "Tag!",
+                                 tag_names: "one two" },
+                      ticket_id: ticket.id }
+      ticket.reload
+      expect(ticket.tags).to be_empty
     end
   end
 end
