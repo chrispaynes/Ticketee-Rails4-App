c52e90ee5587f59c10f68f83ecc9f4e0a10bc881
chrispaynes <cdpaynes@gmail.com>
2016-07-28 21:14:52 -0500

    Users may now search for tickets by state or tag

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -5,6 +5,7 @@ class Ticket < ActiveRecord::Base
   has_many :attachments, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_and_belongs_to_many :tags, uniq: true
+  
   attr_accessor :tag_names
 
   validates :name, presence: true
@@ -12,8 +13,11 @@ class Ticket < ActiveRecord::Base
 
   accepts_nested_attributes_for :attachments, reject_if: :all_blank
 
+  before_create :assign_default_state
+
   searcher do
     label :tag, from: :tags, field: "name"
+    label :state, from: :state, field: "name"
   end
 
   def tag_names=(names)
@@ -21,5 +25,11 @@ class Ticket < ActiveRecord::Base
     names.split.each do |name|
       self.tags << Tag.find_or_initialize_by(name: name)
     end
+  end
+
+  private
+
+  def assign_default_state
+    self.state ||= State.default
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160723005052) do
+ActiveRecord::Schema.define(version: 20160729020903) do
 
   create_table "attachments", force: :cascade do |t|
     t.string   "file"
@@ -55,8 +55,9 @@ ActiveRecord::Schema.define(version: 20160723005052) do
   add_index "roles", ["user_id"], name: "index_roles_on_user_id"
 
   create_table "states", force: :cascade do |t|
-    t.string "name"
-    t.string "color"
+    t.string  "name"
+    t.string  "color"
+    t.boolean "default", default: false
   end
 
   create_table "tags", force: :cascade do |t|
